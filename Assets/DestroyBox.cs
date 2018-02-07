38c81d3fc6c58a647fa56ba25fc4425a5a5126e5
Seth Florman <18.seth.florman@ksd.org>
2018-02-07 13:53:13 -0800

    adding objects and making snap zones

---
+++ Assets/DestroyBox.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBox : MonoBehaviour {
+
+    // Use this for initialization
+
+    
+    public GameObject axe;
+    public int boxHealth;
+
+	void Start () {
+
+        
+        PlayerPrefs.SetInt("boxHealth", boxHealth);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if(collision.gameObject.tag == "pickaxe")
+        {
+
+
+            PlayerPrefs.SetInt("boxHealth", PlayerPrefs.GetInt("boxHealth") - 2);
+
+
+        }
+
+    }
+
+}
---
+++ Assets/SavingHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavingHealth : MonoBehaviour {
+
+    // Use this for initialization
+
+    public int toxins;
+    public GameObject pillBottle;
+    
+
+	void Start () {
+
+        PlayerPrefs.SetInt("Toxins", toxins);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+	}
+
+
+   public void OnCollisionEnter(Collision collision)
+    {
+
+        if(collision.gameObject.tag == "health")
+        {
+
+            PlayerPrefs.SetInt("Toxins", PlayerPrefs.GetInt("Toxins") - 15);
+            Destroy(pillBottle);
+            
+
+        }
+
+
+
+    }
+
+}
